115e4d7de642ff117f8c422f84b5000e3b1e6fbf
Souhiaeb Askri <souhaib2.askri@gmail.com>
2026-01-30 00:12:02 +0100

    add content

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -44,8 +44,9 @@ export function Navbar() {
         <div className="flex h-16 items-center justify-between">
           {/* Logo */}
           <Link href="/" className="flex items-center gap-2">
-            <div className="flex h-9 w-9 items-center justify-center rounded-lg bg-green-600">
-              <Sprout className="h-5 w-5 text-white" />
+            <div className="flex h-9 w-9 items-center justify-center rounded-lg ">
+                <img src="/logo.png" alt="Logo" />
+              {/* <Sprout className="h-5 w-5 text-white" /> */}
             </div>
             <span className="text-xl font-bold text-gray-900">
               {siteConfig.name}
